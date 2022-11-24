088737b3dec8993b97c985cd19d76cff758d86dc
TriekMan <112871935+TriekMan@users.noreply.github.com>
2022-11-24 22:26:58 +0100

    opdracht1.2php

--- opdracht_1.2/index.php
+++ opdracht_1.2/index.php
@@ -12,13 +12,20 @@
     $dagvandaag = date("l");
     $dagjaar = date("z");
     $dagweek = date("w");
-    $maanddagen = date("");
+    $maanddagen = date("t");
     $maand = date("F");
+    $jaar = date("Y");
 
     echo "<p>Het is vandaag: $vandaag.</p>";
     echo "<p>Vandaag is het de " . $dagjaar . "e dag van het jaar.</p>";
     echo "<p>$dagvandaag is de " . $dagweek . "e dag van de week.</p>";
-    echo "<p>De maand $maand heeft in totaal $date</p>";
+    echo "<p>De maand $maand heeft in totaal $maanddagen dagen.</p>";
+    if($jaar % 4 == 0){
+    echo "<p>Het jaar $jaar is een schrikkeljaar.</p>";
+    }
+    else{
+    echo "<p>Het jaar $jaar is geen schrikkeljaar.</p>";
+    }
     ?>
 </body>
 </html>
